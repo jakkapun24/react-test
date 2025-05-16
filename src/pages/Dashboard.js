2c05d8d9adf8f21ad99438614226568ecad63488
jakka <jakkapun.wasupho24@gmail.com>
2025-05-16 16:17:29 +0700

    update

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 export default function Dashboard() {
+
+
+   const [data, setData] = useState([
+
+    {
+      id:'1',
+      title:'data'
+    },
+     {
+      id:'2',
+      title:'map'
+    },
+   ])
+
+  // useEffect(() => {
+  //   const getPosts = async () => {
+  //     const res = await fetch('api/powermeter')
+  //     const data = await res.json()
+  //     setData(data)
+  //   }
+
+  //   getPosts()
+  // }, [])
+
+console.log(data)
+
   return (
+    <>
     <div>dashboard</div>
+    {data.map((post) => (
+        <p key={post.id}>{post.title}</p>
+      ))}
+      </>
   )
 }
